dded9491ce7ba21efd2c19b93eadf77ef9964033
Shuyuan Fan <shuyuan.fan@tuhh.de>
2023-03-31 11:35:35 +0000

    Update fcs.m

--- Matlab files/functions/fcs.m
+++ Matlab files/functions/fcs.m
@@ -4,6 +4,15 @@
 % Institute: ICS of TUHH
 % shuyuan.fan@tuhh.de
 
+%---------------------------------------------------------------------------------------------
+% For Paper, 
+% "Closed-loop Concave Configurations and Concave Quadratic Regulator"
+% by Shuyuan Fan and Herbert Werner
+% Copyright (c) Institute of Control Systems, Hamburg University of Technology. All rights reserved.
+% Licensed under the GPLv3. See LICENSE in the project root for license information.
+% Author(s): Shuyuan Fan
+%--------------------------------------------------------------------------------------------
+
 function fc = fcs(rho,kmin,kmax,beta,eta)
 %FCS simplified concave factor
 % fc = (beta*kmin*rho+eta*kmax)/(beta*rho+eta)
