dc6605bd99cf3f84e43d7d9423f1ea24d24b6c96
Shuyuan Fan <shuyuan.fan@tuhh.de>
2023-03-31 11:11:43 +0000

    Upload New File

---
+++ Matlab files/parameter/case1.m
@@ -0,0 +1,67 @@
+%% CASE 1
+%% Name: Parameters of Case 1
+% Description: This file contains all the parameters of Case 1
+% Designer: Shuyuan Fan
+% Institute: ICS of TUHH
+% shuyuan.fan@tuhh.de
+
+clc
+
+% system matrix
+A = [-1 1;-1 1];
+B = [1, 1; 2,1];
+
+% baseline controller
+Qb = 2*eye(2);
+Rb = 2*eye(2);
+[Kb,Pb,~] = lqr(A,B,Qb,Rb);
+Aclb = A-B*Kb;
+%[T,Lamb] = jordan(Aclb);
+[T,Lamb] = eig(Aclb);
+T = inv(T);
+% parameters of concave factor for concave configuration
+k1min = 0.4; k1max = 8; beta1 = 1; eta1 = 0.1;
+k2min = 0.2; k2max = 2;beta2 = 1;eta2 = 0.1;
+
+Fcc = [k1min,k1max,beta1,eta1,k2min,k2max,beta2,eta2];
+
+% parameters for CQR
+q1min = 0.01; q1max = 50; betaq1 = 10; etaq1 = 0.02;
+q2min = 0.02; q2max = 20; betaq2 = 10; etaq2 =0.1;
+R = eye(2);
+Fcqr = [q1min,q1max,betaq1,etaq1,q2min,q2max,betaq2,etaq2];
+
+Qc0 = diag([q1max,q2max]);
+Rc0 = eye(2);
+
+[Kc0,Pc0,~] = lqr(A,B,Qc0,Rc0);
+
+% calculate P(\rho)
+rhospan = 0:0.001:10;
+
+Pc0_r = reshape(Pc0',[4,1]);
+Fcqr = reshape(Fcqr,[8,1]);
+Pc0_r_ag = [Pc0_r;Fcqr];
+
+[rho,P]=ode45(@dpdx_case1,rhospan,Pc0_r_ag);
+
+% initial states
+x0 = [2;1];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
